d0ccc99adbc05fbb402b0a01a83cf700d80dfd8e
MaxHolzmann <max@holzmann.io>
2024-12-13 21:24:14 -0600

    Designs create workout template page more

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -9,23 +9,47 @@ async function fetchHello() {
 
 export default function HomeScreen() {
   return (
-    <div className="">
+    <div className="flex flex-col-reverse">
       <div className="text-center text-white">
         <h1 className="text-white text-6xl">Create a Workout Template</h1>
-        <form className="text-black bg-white rounded-md">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
-          <label htmlFor="day">Day</label>
-          <select>
+        <form className="text-black bg-white rounded-xl flex flex-col items-center justify-center m-10">
+          <label htmlFor="title">Workout Name</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            className=" border-black border-2 shadow-sm rounded-md"
+          />
+          <label htmlFor="day">Split Name</label>
+          <select className=" border-black border-2 shadow-sm rounded-md">
             <option value="push">Push</option>
             <option value="pull">Pull</option>
             <option value="legs">Legs</option>
           </select>
-          <label htmlFor="exercises">Exercises</label>
-          <input type="checkbox" id="exercises" name="exercises" />
-          <input type="checkbox" id="exercises" name="exercises" />
-          <input type="checkbox" id="exercises" name="exercises" />
-          <input type="checkbox" id="exercises" name="exercises" />
+          <div className="flex flex-col">
+            <label htmlFor="exercises">Exercises</label>
+
+            <div className="flex flex-col bg-gray-200 rounded-md p-2 m-4">
+              <div>
+                <h2 className="text-xl">Exercise {1}</h2>
+                <select className=" border-black border-2 shadow-sm rounded-md">
+                  <option value="push">Push</option>
+                  <option value="pull">Pull</option>
+                  <option value="legs">Legs</option>
+                </select>
+              </div>
+              <p>Reps</p>
+              <input
+                className=" border-black border-2 shadow-sm rounded-md"
+                type="number"
+              ></input>
+              <p>Weight</p>
+              <input
+                className=" border-black border-2 shadow-sm rounded-md"
+                type="number"
+              ></input>
+            </div>
+          </div>
         </form>
       </div>
       {/* <Button onPress={() => fetchHello()} title="Fetch hello" />; */}
